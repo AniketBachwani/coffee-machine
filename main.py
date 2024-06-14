fd7bfdbc6009935d6cc1116e09546985450303d9
unknown <aniketbachwani0@gamil.com>
2024-06-14 17:39:25 +0530

    first

---
+++ main.py
@@ -0,0 +1,20 @@
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+menu = Menu()
+coffeemaker = CoffeeMaker()
+money_machine = MoneyMachine()
+is_on = True
+while is_on:
+    options = menu.get_items()
+    choice = input(f"what do you like to have? {options}")
+    if choice == "report":
+        coffeemaker.report()
+        money_machine.report()
+    elif choice == "off":
+        is_on = False
+    else:
+        drink = menu.find_drink(choice)
+        if coffeemaker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffeemaker.make_coffee(drink)
